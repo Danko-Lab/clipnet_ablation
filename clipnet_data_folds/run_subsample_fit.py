53317858755fd6c33005cef4f76816aa8a650282
Adam Youlin He <ayh8@cornell.edu>
2024-02-22 02:21:44 -0500

    Update run_subsample_fit.py

--- clipnet_data_folds/run_subsample_fit.py
+++ clipnet_data_folds/run_subsample_fit.py
@@ -1,7 +1,7 @@
 import os
 
 clipnet_install = "/home2/ayh8/clipnet"
-model_root = "/home2/ayh8/subsample_models/n10_run0"
+model_root = "/home2/ayh8/subsample_models/n5_run0"
 gpu = 0
 for i in range(9):
     cmd = f"python {clipnet_install}/fit_nn.py {model_root}/f{i + 1} --use_specific_gpu {gpu}"
