8327502948db21692c32ea3e86e9422715a2dd07
Adam Youlin He <ayh8@cornell.edu>
2024-02-22 01:04:12 -0500

    I think there was an error somewhere?

---
+++ clipnet_data_folds/run_subsample_fit.py
@@ -0,0 +1,9 @@
+import os
+
+clipnet_install = "/home2/ayh8/clipnet"
+model_root = "/home2/ayh8/subsample_models/n10_run0"
+gpu = 0
+for i in range(9):
+    cmd = f"python {clipnet_install}/fit_nn.py {model_root}/f{i + 1} --use_specific_gpu {gpu}"
+    os.system(f"echo {cmd}")
+    os.system(cmd)
