4b506a3715f95cf2950635d73f0225561606aa43
Adam Youlin He <ayh8@cornell.edu>
2024-02-26 20:42:17 -0500

    Update run_subsample_fit.py

--- clipnet_data_folds/run_subsample_fit.py
+++ clipnet_data_folds/run_subsample_fit.py
@@ -1,9 +1,14 @@
 import os
+import multiprocessing as mp
 
-clipnet_install = "/home2/ayh8/clipnet"
-model_root = "/home2/ayh8/subsample_models/n5_run0"
-gpu = 0
-for i in range(9):
+def run_fitting_cmd(i):
+    clipnet_install = "/home2/ayh8/clipnet"
+    model_root = "/home2/ayh8/subsample_models/n30_run0"
+    gpu = i % 2
     cmd = f"python {clipnet_install}/fit_nn.py {model_root}/f{i + 1} --use_specific_gpu {gpu}"
     os.system(f"echo {cmd}")
-    os.system(cmd)
+    #os.system(cmd)
+
+with mp.Pool(2) as p:
+    p.map(run_fitting_cmd, range(9))
+    
