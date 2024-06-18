85232ab9b0a1755f59ddd7f53cd230cdf596a2a8
Adam Youlin He <ayh8@cornell.edu>
2024-06-17 22:05:28 -0400

    added example plots

---
+++ example_predictions/README.md
@@ -0,0 +1,87 @@
+# Extracting example windows
+
+This directory contains scripts for extracting sample windows.
+
+```bash
+cat lcl_cre_example_windows.bed qtl_snps_windows.bed | bedtools sort > example_windows.bed
+```
+
+Run the snakemake pipelines in `procap`, and `sequence` to extract data for each of the windows in `example_windows.bed`.
+
+Then, run the prediction using:
+
+```bash
+cd /home2/ayh8/clipnet/
+conda activate clipnet
+```
+
+Individual models:
+
+```python
+import itertools
+import os
+
+n_individuals = [5, 10, 15, 20, 30]
+runs = range(5)
+
+for n, r in itertools.product(n_individuals, runs):
+    run = f"n{n}_run{r}"
+    for f in range(1, 10):
+        cmd = f"python predict_individual_model.py \
+            ../clipnet_subsampling/models/{run}/fold_{f}.h5 \
+            ../data/lcl/examples/concat_sequence.fna.gz \
+            ../predictions/lcl_subsample/examples/{run}_fold_{f}_examples_prediction.h5 \
+            --gpu 0"
+        print(cmd)
+        os.system(cmd)
+```
+
+Ensemble models:
+
+```python
+import itertools
+import os
+
+n_individuals = [5, 10, 15, 20, 30]
+runs = range(5)
+
+for n, r in itertools.product(n_individuals, runs):
+    run = f"n{n}_run{r}"
+    cmd = f"python predict_ensemble.py \
+        ../data/lcl/examples/concat_sequence.fna.gz \
+        ../predictions/lcl_subsample/examples/{run}_ensemble_examples_prediction.h5 \
+        --model_dir ../clipnet_subsampling/models/{run} \
+        --gpu 0"
+    print(cmd)
+    os.system(cmd)
+```
+
+Calculate DeepSHAP scores from model ensembles:
+
+```python
+import itertools
+import os
+
+n_individuals = [5, 10, 15, 20, 30]
+runs = range(5)
+
+for n, r in itertools.product(n_individuals, runs):
+    run = f"n{n}_run{r}"
+    cmd = f"python calculate_deepshap.py \
+        ../data/lcl/examples/concat_sequence.fna.gz \
+        ../data/attribution_scores/examples_deepshap_{run}.npz \
+        ../data/test_onehot.npz \
+        --mode quantity \
+        --gpu"
+    print(cmd)
+    os.system(cmd)
+```
+
+```bash
+python /home2/ayh8/clipnet/calculate_deepshap.py \
+    /home2/ayh8/data/lcl/examples/concat_sequence.fna.gz \
+    /home2/ayh8/data/attribution_scores/examples_deepshap_quantity.npz \
+    data/test_onehot.npz \
+    --mode quantity \
+    --gpu
+```
---
+++ example_predictions/folds_shap_sum_node_examples.py
@@ -0,0 +1,132 @@
+"""
+Calculate deeplift contribution scores using shap.DeepExplainer.
+"""
+
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import pyfastx
+import shap
+import tensorflow as tf
+import ushuffle
+
+sys.path.append("/home2/ayh8/clipnet_scripts/clipnet/")
+import utils
+
+import clipnet
+
+# This will fix an error message for running tf.__version__==2.5
+shap.explainers._deep.deep_tf.op_handlers[
+    "AddV2"
+] = shap.explainers._deep.deep_tf.passthrough
+tf.compat.v1.disable_v2_behavior()
+
+
+np.random.seed(617)
+n_subset = 100
+FOLDS = range(1, 10)
+
+# Load sequences
+ref_fp = os.path.join(
+    "/home2/ayh8/data/gse110638/tfbs_sampling/",
+    "random_5000/random_tss_windows_reference_seq.fna.gz",
+)
+ref_seqs = pyfastx.Fasta(ref_fp)
+
+# Perform dinucleotide shuffle on 100 random sequences
+reference = [
+    ref_seqs[i]
+    for i in np.random.choice(
+        np.array(range(len(ref_seqs))), size=n_subset, replace=False
+    )
+]
+shuffled_ref = [ushuffle.shuffle(rec.seq, 1000, 2) for rec in reference]
+
+# One-hot encode shuffled sequences and select 5000 to use as reference
+onehot_ref = np.array([utils.OneHotDNA(seq).onehot for seq in shuffled_ref])
+
+# Load target sequences
+explain_fasta_fp = os.path.join(
+    "/home2/ayh8/data/gse110638/examples/", "examples_reference_seq.fna.gz"
+)
+seqs_to_explain = utils.get_onehot_fasta_sequences(explain_fasta_fp)
+
+# Load models and create explainers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+nn = clipnet.CLIPNET(n_gpus=0)
+models = [
+    tf.keras.models.load_model(
+        f"/home2/ayh8/ensemble_models/fold_{i}.h5", compile=False
+    )
+    for i in FOLDS
+]
+model_fps = list(Path("/home2/ayh8/ensemble_models/").glob("*.h5"))
+models = [tf.keras.models.load_model(fp, compile=False) for fp in model_fps]
+explainers = [
+    shap.DeepExplainer((model.input, model.output[1]), onehot_ref) for model in models
+]
+
+# Main explanations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+raw_explanations = [explainer.shap_values(seqs_to_explain) for explainer in explainers]
+scaled_explanations = {
+    f"fold_{i}": (np.sum(raw_explanations[i - 1], axis=0) * seqs_to_explain).swapaxes(
+        1, 2
+    )
+    for i in FOLDS
+}
+
+out_dir = Path("/home2/ayh8/attribution_scores/").joinpath("examples")
+out_dir.mkdir(exist_ok=True, parents=True)
+
+np.savez_compressed(
+    out_dir.joinpath("folds_examples_sum_node.npz"),
+    **scaled_explanations,
+)
+
+np.savez_compressed(
+    out_dir.joinpath("folds_examples_seqs.npz"),
+    (seqs_to_explain / 2).astype(int).swapaxes(1, 2),
+)
+
+# QTL explanations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+qtls_to_explain = np.array(
+    [
+        # rs185220 (chr5:56909530)
+        seqs_to_explain[51],
+        seqs_to_explain[51],
+        # rs8050061 (chr16:80231939)
+        seqs_to_explain[19],
+        seqs_to_explain[19],
+        # rs1016110 (chr22:42440331)
+        seqs_to_explain[47],
+        seqs_to_explain[47],
+    ]
+)
+# rs185220 (chr5:56909530) Convert minor/reference allele (A) to major allele (G)
+qtls_to_explain[0, 500, :] = np.array([0, 0, 2, 0])
+# rs8050061 (chr16:80231939) Convert major/reference allele (T) to minor allele (C)
+qtls_to_explain[2, 500, :] = np.array([0, 2, 0, 0])
+# rs1016110 (chr22:42440331) Convert major/reference allele (A) to minor allele (C)
+qtls_to_explain[4, 499, :] = np.array([0, 2, 0, 0])
+
+raw_qtl_explanations = [
+    explainer.shap_values(qtls_to_explain) for explainer in explainers
+]
+scaled_qtl_explanations = {
+    f"fold_{i}": (
+        np.sum(raw_qtl_explanations[i - 1], axis=0) * qtls_to_explain
+    ).swapaxes(1, 2)
+    for i in FOLDS
+}
+
+out_dir = Path("/home2/ayh8/attribution_scores/").joinpath("examples")
+out_dir.mkdir(exist_ok=True, parents=True)
+
+np.savez_compressed(
+    out_dir.joinpath("folds_qtl_examples_sum_node.npz"),
+    **scaled_qtl_explanations,
+)
